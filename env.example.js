4d2220decbc4393f3bb7d87649a0de6f057d3d88
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-01 13:40:25 +0300

    Initial commit

---
+++ env.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+    GOOGLE_KEY: "[your Google API key]",
+    WRLD_KEY: "[your WRLD API key]",
+}
---
+++ src/app.js
@@ -0,0 +1,6 @@
+const Wrld = require("wrld.js");
+const env = require("./env")
+
+const keys = {
+    google: env.GOOGLE_KEY,
+}
