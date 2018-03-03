d771b02f872cea332c2a59e1f598a8416d5548bf
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-03 18:07:53 +0300

    Implemented automatic indoor map launch

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
-const Wrld = require('wrld.js')
-const env = require('../env')
+const Wrld = require('wrld.js');
+const env = require('../env');
 
 const keys = {
-  wrld: env.WRLD_KEY
-}
+  wrld: env.WRLD_KEY,
+};
+
+const overgateId = 'EIM-e16a94b1-f64f-41ed-a3c6-8397d9cfe607';
 
 window.addEventListener('load', async () => {
+  const mallBtn = document.getElementById('mallBtn');
+  const exitBtn = document.getElementById('mallBtn');
+
   const map = Wrld.map('map', keys.wrld, {
     center: [56.459733, -2.973371],
     zoom: 17,
-    indoorsEnabled: true
-  })
+    indoorsEnabled: true,
+  });
 
-  map.on('load', () => {
-    map.indoors.enter('Overgate');
-    console.log('go in');
-  })
-})
+  mallBtn.addEventListener('click', () => {
+    map.indoors.enter(overgateId);
+  });
+
+  exitBtn.addEventListener('click', () => {
+    map.indoors.exit();
+  });
+
+  map.on('initialstreamingcomplete', () => {
+    map.indoors.enter(overgateId);
+  });
+
+  map.indoors.on('indoormapenter', (event) => {
+    if (event.indoorMap.getIndoorMapId() === overgateId) {
+      map.indoors.setFloor(0);
+      map.setView([56.458968, -2.973841], 18);
+    }
+  });
+});
