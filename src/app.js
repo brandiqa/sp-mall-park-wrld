54d52809d399c0d323de2bd989cead9e2f221f98
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-03 12:58:12 +0300

    Configured babel to support async

--- src/app.js
+++ src/app.js
@@ -5,3 +5,17 @@ const keys = {
     wrld: env.WRLD_KEY,
 }
 
+window.addEventListener("load", async() => {
+
+    const map = L.Wrld.map("map", keys.wrld, {
+        center: [56.459733, -2.973371],
+        zoom: 17,
+        indoorsEnabled: true
+    });
+
+    map.on('load', () => {
+        map.indoors.enter("Overgate");
+        console.log('go in');
+    });
+});
+
