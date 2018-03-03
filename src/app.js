638da1bfc3a9bbac1be78608db6c22a0cb555926
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-03 13:11:09 +0300

    fixed eslint issues

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
-const Wrld = require("wrld.js");
-const env = require("../env");
+const Wrld = require('wrld.js')
+const env = require('../env')
 
 const keys = {
-    wrld: env.WRLD_KEY,
+  wrld: env.WRLD_KEY
 }
 
-window.addEventListener("load", async() => {
+window.addEventListener('load', async () => {
+  const map = Wrld.map('map', keys.wrld, {
+    center: [56.459733, -2.973371],
+    zoom: 17,
+    indoorsEnabled: true
+  })
 
-    const map = L.Wrld.map("map", keys.wrld, {
-        center: [56.459733, -2.973371],
-        zoom: 17,
-        indoorsEnabled: true
-    });
-
-    map.on('load', () => {
-        map.indoors.enter("Overgate");
-        console.log('go in');
-    });
-});
-
+  map.on('load', () => {
+    map.indoors.enter('Overgate');
+    console.log('go in');
+  })
+})
