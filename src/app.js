8195ad98d395e78c75a5bfc0167fa157bbac87dc
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-05 20:24:46 +0300

    Added floor buttons and indoor marker

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ const overgateId = 'EIM-e16a94b1-f64f-41ed-a3c6-8397d9cfe607';
 
 window.addEventListener('load', async () => {
   const mallBtn = document.getElementById('mallBtn');
-  const exitBtn = document.getElementById('mallBtn');
+  const exitBtn = document.getElementById('exitBtn');
+  const topBtn = document.getElementById('topBtn');
+  const upBtn = document.getElementById('upBtn');
+  const downBtn = document.getElementById('downBtn');
+  const bottomBtn = document.getElementById('bottomBtn');
 
   const map = Wrld.map('map', keys.wrld, {
     center: [56.459733, -2.973371],
@@ -25,6 +29,26 @@ window.addEventListener('load', async () => {
     map.indoors.exit();
   });
 
+  topBtn.addEventListener('click', () => {
+    const indoorMap = map.indoors.getActiveIndoorMap();
+
+    if (indoorMap) {
+      map.indoors.setFloor(indoorMap.getFloorCount() - 1);
+    }
+  });
+
+  upBtn.addEventListener('click', () => {
+    map.indoors.moveUp();
+  });
+
+  downBtn.addEventListener('click', () => {
+    map.indoors.moveDown();
+  });
+
+  bottomBtn.addEventListener('click', () => {
+    map.indoors.setFloor(0);
+  });
+
   map.on('initialstreamingcomplete', () => {
     map.indoors.enter(overgateId);
   });
@@ -35,6 +59,15 @@ window.addEventListener('load', async () => {
       map.setView([56.458968, -2.973841], 18);
       /* eslint-disable no-unused-vars */
       const indoorControl = new WrldIndoorControl('widget-container', map);
+
+      const indoorMapId = event.indoorMap.getIndoorMapId();
+      const buildingName = event.indoorMap.getIndoorMapName();
+
+      const marker = Wrld.marker([56.4593862, -2.9742504], {
+        title: 'JD Sports',
+        indoorMapId,
+        indoorMapFloorId: 1,
+      }).addTo(map);
     }
   });
 });
