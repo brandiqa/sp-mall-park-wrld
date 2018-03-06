fd73069c889aabf627d1b1a213ea9565090ce6a2
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-06 19:45:28 +0300

    Implemented getPois function

--- src/poi.js
+++ src/poi.js
@@ -1,6 +1,20 @@
+const axios = require('axios');
+
+const client = axios.create({
+  baseURL: 'http://127.0.0.1:3000',
+  timeout: 1000,
+});
+
+const message = document.getElementById('message');
+
 module.exports = {
-  getPOIs: () => {
-    const pois = [];
-    return pois;
+  getPOIs: async () => {
+    try {
+      const pois = await client.get('/pois');
+      return pois;
+    } catch (error) {
+      message.innerHTML = `An error occurred: ${error}`;
+    }
+    return [];
   },
 };
