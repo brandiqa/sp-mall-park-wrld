c212017dbd216c398ebb291c3133bd3e3a9f96fc
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-06 22:39:14 +0300

    Implemented POI popup template

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const keys = {
 
 const indoorMapId = 'EIM-e16a94b1-f64f-41ed-a3c6-8397d9cfe607';
 
+const template = $.templates('#cardStoreTemplate');
+
+const popupOptions = {
+  indoorMapId,
+  indoorMapFloorIndex: 0,
+  autoClose: false,
+  closeOnClick: true,
+};
+
 window.addEventListener('load', async () => {
   const map = await Wrld.map('map', keys.wrld, {
     center: [56.459733, -2.973371],
@@ -31,6 +40,12 @@ window.addEventListener('load', async () => {
         indoorMapId,
         indoorMapFloorId: 1,
       }).addTo(map);
+      // Render poi data to jsrender template
+      const htmlOutput = template.render(poi);
+      // Create and bind popup to marker
+      const popup = Wrld.popup(popupOptions)
+        .setContent(htmlOutput);
+      marker.bindPopup(popup);
     }
   };
 
@@ -49,24 +64,6 @@ window.addEventListener('load', async () => {
 
       const pois = await getPOIs();
       placeMarkers(pois);
-
-      // const popupOptions = {
-      //   indoorMapId,
-      //   indoorMapFloorIndex: 0,
-      //   autoClose: false,
-      //   closeOnClick: true,
-      // };
-
-      // const template = $.templates('#cardStoreTemplate');
-      // const data = {
-      //   title: 'J.D. Sports',
-      // };
-      // const htmlOutput = template.render(data);
-
-      // const popup = Wrld.popup(popupOptions)
-      //   .setContent(htmlOutput);
-
-      // marker.bindPopup(popup);
     }
   });
 });
