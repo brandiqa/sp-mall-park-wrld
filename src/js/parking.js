d29dd54b10b75dd81420285330e89ded98fffeff
Michael Wanyoike <micwanyoike@gmail.com>
2018-03-21 13:17:37 +0300

    Defined parking area

--- src/js/parking.js
+++ src/js/parking.js
@@ -15,4 +15,14 @@ window.addEventListener('load', async () => {
   setTimeout(() => {
     map.setCameraHeadingDegrees(45).setCameraTiltDegrees(20);
   }, 1000);
+
+  // Highlight parking areas
+  const polygonPoints1 = [
+    [56.459857, -2.974004],
+    [56.459889, -2.974036],
+    [56.459836, -2.974188],
+    [56.460079, -2.974526],
+    [56.460254, -2.974096],
+    [56.459954, -2.973698]];
+  const parking1 = Wrld.polygon(polygonPoints1).addTo(map);
 });
